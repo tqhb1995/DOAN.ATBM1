6bae41b17c1640421c11436a218bf6ac7de43d6a
tqhb1995 <1412553@student.hcmus.edu.vn>
2017-06-28 22:43:56 +0700

    update proc

--- scipts.sql
+++ scipts.sql
@@ -339,3 +339,52 @@ end;
 
 
 GRANT SELECT ON VIEW_DEAN TO p_NHANVIEN_PKD, p_NHANVIEN_PKH, p_NHANVIEN_THDA, p_TRUONGPHONG, p_GIAMDOC;
+
+
+
+--------------PROC INSERT, UPDATE, DELETE trên DEAN cho nhân viên phòng kế hoạch----------
+
+---PROC INSERT DEAN---
+CREATE OR REPLACE PROCEDURE INSERT_DEAN(  
+p_mada DEAN.MADA%TYPE,  
+p_tenda DEAN.TENDA%TYPE,  
+p_ngaybd DEAN.NGAYBD%TYPE,
+p_phong DEAN.PHONG%TYPE,
+p_diadiem_da DEAN.DIADIEM_DA%TYPE)
+IS  
+BEGIN  
+INSERT INTO DEAN (MADA, TENDA, NGAYBD, PHONG, DIADIEM_DA)
+      VALUES (p_mada, p_tenda, p_ngaybd, p_phong, p_diadiem_da);
+COMMIT;  
+END; 
+
+
+
+---UPDATE PROC DEAN---
+CREATE OR REPLACE PROCEDURE UPDATE_DEAN 
+(   
+  p_mada DEAN.MADA%TYPE,  
+  p_tenda DEAN.TENDA%TYPE,  
+  p_ngaybd DEAN.NGAYBD%TYPE,
+  p_phong DEAN.PHONG%TYPE,
+  p_diadiem_da DEAN.DIADIEM_DA%TYPE
+)
+is
+BEGIN  
+  
+  UPDATE DEAN 
+  SET MADA = p_mada, TENDA = p_tenda, NGAYBD = p_ngaybd, PHONG = p_phong, DIADIEM_DA = p_diadiem_da 
+  WHERE MADA = p_mada;  
+  COMMIT; 
+END; 
+
+---DELETE PROC DEAN--
+CREATE OR REPLACE PROCEDURE DELETE_DEAN 
+(   
+  p_mada IN DEAN.MADA%TYPE
+)  
+IS  
+BEGIN  
+  DELETE FROM DEAN WHERE MADA = p_mada;  
+END;
+
