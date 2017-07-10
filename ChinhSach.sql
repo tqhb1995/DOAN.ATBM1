daf1acb422c0cb088c6ffcb9103ca20c6e071885
tqhb1995 <1412553@student.hcmus.edu.vn>
2017-07-10 16:25:01 +0700

    upate

--- ChinhSach.sql
+++ ChinhSach.sql
@@ -14,7 +14,7 @@ begin
     TEMP :=  'MANV = ''' || USER_NAME || '''';
     return TEMP;
 end;
----Tạo hàm chính sách để nhân viên chỉ có thể xem trường lương và phụ cấp của chính họ mà thôi
+---Tạo hàm chính sách để nhân viên chỉ có thể xem trường lương và phụ cấp của chính họ mà thôi--
 BEGIN
 DBMS_RLS.add_policy
 (object_schema => 'DOAN',
@@ -133,3 +133,56 @@ BEGIN
 
   commit;
 END;
+
+
+--------===============================
+--MÃ HÓA LƯƠNG---
+--1. Tạo function--
+CREATE OR REPLACE PACKAGE ENCRYPT_DECRYPT
+AS
+  FUNCTION ENCRYPT_LUONG(p_in IN varchar2,p_key IN VARCHAR2) 
+    RETURN RAW DETERMINISTIC;
+  FUNCTION DECRYPT_LUONG(p_in IN RAW,p_key IN VARCHAR2) 
+    RETURN varchar2 DETERMINISTIC;
+END ENCRYPT_DECRYPT;
+
+
+--2. CAI DAT 2 FUNCTION TREN
+CREATE OR REPLACE PACKAGE BODY ENCRYPT_DECRYPT  
+IS 
+  encryption_type PLS_INTEGER :=
+							 DBMS_CRYPTO.ENCRYPT_DES
+							+DBMS_CRYPTO.CHAIN_CBC
+							+DBMS_CRYPTO.PAD_PKCS5;
+  FUNCTION ENCRYPT_LUONG(p_in IN varchar2,p_key IN VARCHAR2) 
+    RETURN RAW DETERMINISTIC
+  IS
+  	encrypted_raw raw(2000);
+  BEGIN
+       encrypted_raw := dbms_crypto.encrypt(
+          src => utl_raw.cast_to_raw(p_in),
+          typ => encryption_type,
+          key => utl_raw.cast_to_raw(p_key)
+      );
+      return encrypted_raw;
+  END ENCRYPT_LUONG;
+  
+  FUNCTION DECRYPT_LUONG(p_in IN RAW,p_key IN VARCHAR2) 
+    RETURN varchar2 DETERMINISTIC
+  IS
+    decrypted_raw raw(2000);
+  BEGIN
+
+       decrypted_raw := dbms_crypto.decrypt(
+          src => p_in,
+          typ => encryption_type,
+          key => utl_raw.cast_to_raw(p_key)
+      );
+      
+      return utl_raw.cast_to_varchar2(decrypted_raw);
+  END DECRYPT_LUONG;
+END ENCRYPT_DECRYPT;
+
+UPDATE MAHOA_LUONG SET MAHOA_LUONG.HASHLUONG=ENC_DEC.ENCRYPT_SALARY(HASHLUONG,MANV||MANV);
+UPDATE MAHOA_LUONG1 SET MAHOA_LUONG1.HASHLUONG=ENC_DEC.DECRYPT_SALARY(HASHLUONG,MANV||MANV);
+select MANV, HONV, TENLOT, TENV, HASHLUONG from MAHOA_LUONG;
